ddc389fc1205ea96dfb36dac7348620ddecef21a
Zaksofon <tsvyatkovhr@gmail.com>
2022-03-15 15:45:49 +0200

    Project added with Database Model files (Plot and Crop); Added Migratoins;

---
+++ SprayLog/Data/Migrations/20220315122720_PlotAndCropTables.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace SprayLog.Data.Migrations
+{
+    public partial class PlotAndCropTables : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Plots",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    PlotName = table.Column<string>(type: "nvarchar(4)", maxLength: 4, nullable: false),
+                    CropId = table.Column<int>(type: "int", nullable: false),
+                    Area = table.Column<double>(type: "float", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Plots", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Crops",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    CropName = table.Column<string>(type: "nvarchar(4)", maxLength: 4, nullable: false),
+                    PlantedOn = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    Batch = table.Column<int>(type: "int", nullable: false),
+                    PlotId = table.Column<int>(type: "int", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Crops", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Crops_Plots_PlotId",
+                        column: x => x.PlotId,
+                        principalTable: "Plots",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Restrict);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Crops_PlotId",
+                table: "Crops",
+                column: "PlotId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Plots_CropId",
+                table: "Plots",
+                column: "CropId");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Plots_Crops_CropId",
+                table: "Plots",
+                column: "CropId",
+                principalTable: "Crops",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Crops_Plots_PlotId",
+                table: "Crops");
+
+            migrationBuilder.DropTable(
+                name: "Plots");
+
+            migrationBuilder.DropTable(
+                name: "Crops");
+        }
+    }
+}
---
+++ SprayLog/Data/Models/Crop.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+
+namespace SprayLog.Data.Models
+{
+    public class Crop
+    {
+        public int Id { get; init; }
+
+        [Required]
+        [StringLength(4, MinimumLength = 2)]
+        public string CropName { get; set; }
+
+        public DateTime PlantedOn { get; set; }
+
+        public int Batch { get; set; }
+
+        //[Required]
+        //public int PlotId { get; set; }
+        //public Plot Plot { get; set; }
+
+        //public IEnumerable<Plot> Plots { get; set; } = new List<Plot>();
+    }
+}
