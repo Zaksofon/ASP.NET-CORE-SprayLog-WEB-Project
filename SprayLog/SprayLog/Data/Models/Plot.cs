e0cd742eeb95a35aa7623fa9a99ad898341bb0c9
Zaksofon <tsvyatkovhr@gmail.com>
2022-03-15 16:28:46 +0200

    PesticideType Model added

---
+++ SprayLog/SprayLog/Data/Models/Plot.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Security.Cryptography;
+
+namespace SprayLog.Data.Models
+{
+    public class Plot
+    {
+        public int Id { get; init; }
+
+        [Required]
+        [StringLength(4, MinimumLength = 2)]
+        public string PlotName { get; set; }
+
+        [Required] 
+        public int CropId { get; set; }
+        public Crop Crop { get; set; }
+
+        public double Area { get; set; }
+
+        public IEnumerable<Crop> Crops { get; set; } = new List<Crop>();
+
+    }
+}
---
+++ SprayLog/SprayLog/Data/SprayLogDbContext.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using SprayLog.Data.Models;
+
+namespace SprayLog.Data
+{
+    public class SprayLogDbContext : IdentityDbContext
+    {
+        public SprayLogDbContext(DbContextOptions<SprayLogDbContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<Plot> Plots { get; init; }
+        public DbSet<Crop> Crops { get; init; }
+    }
+}
